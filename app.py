faeb3631bc3c083da6213e9eacb97544d1089d2d
tahjeirac <tahjeirac@users.noreply.github.com>
2020-07-30 01:27:47 -0400

    figure out ajax for sending js data, fixed saving, added alerts for both settings

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import json
 import sqlite3
-#TODO: Fix apperance, add more filtering option (by pack, category), figure out ajax and jquery for sending js dictonary
 from flask import Flask, render_template, request
+"""TODO: Fix apperance, add more filtering option (by pack, category), 
+start off category checkboxes with all chekced by default (create all button)
+change it so not having one section checked doesnt unchechk all
+update methods to jquery so no resetting data
+figure out .then for ajax to open generate
+see if using ajax makes sense for generate ("commente out as template not working)"""
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config['SECRET_KEY'] = 'any secret string'
 
+choices = ""
 
 def dict_factory(cursor, row):
     d = {}
@@ -16,17 +23,15 @@ def dict_factory(cursor, row):
 
 
 @app.route('/', methods=('GET', 'POST'))
-def hello_world():
-   return render_template('home.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/generate', methods=('GET', 'POST'))
 def generate_event():
     conn = sqlite3.connect('events.db')
     conn.row_factory = dict_factory
     cur = conn.cursor()
-    deathIncluded = str((request.args.get('deathStorage')))
-
-
+    deathIncluded = choices['deaths']
     with conn:
         if (not deathIncluded):
             cur.execute("""SELECT * FROM Sims4Test WHERE event IN 
@@ -36,14 +41,26 @@ def generate_event():
                    (SELECT event FROM Sims4Test ORDER BY RANDOM() LIMIT 1) """)
         chosen_event = cur.fetchone()
 
+    print(chosen_event)
     event = chosen_event.get("event", "no event found")
     description = chosen_event.get("description", "no description available")
     category = chosen_event.get("event type", "no category")
 
-    return render_template('displayEvents.html', title='random In', event = event, description = description, category = category)
+    return render_template('home.html', title='random In', event = event, description = description, category = category)
 
+#
+# @app.route('/postmethod', methods = ['POST'])
+# def get_post_javascript_data():
+#     print("hello")
+#     jsdata = request.form['javascript_data']
+#     print(json.loads(jsdata)[0])
 
-
+@app.route('/selection', methods = ['POST'])
+def get_post_javascript_data():
+    global choices
+    jsdata = request.form['javascript_data']
+    choices  = json.loads(jsdata)
+    return "OK"
 
 if __name__ == '__main__':
     app.run()
--- static/JS/main.js
+++ static/JS/main.js
@@ -1,46 +1,107 @@
 var packCheckBoxes = document.getElementsByName("packsIncluded[]");
+var categoryCheckBoxes = document.getElementsByName("categoriesIncluded[]");
+var checkBoxes = document.getElementsByClassName("checkBox")
 
-//send whether deaths are wanted
-function checkCheckBox() {
-    createDictonary();
-    let checkBox = document.getElementById('deaths');
-    document.getElementById('deathStorage').value = checkBox.checked;
-}
 
-function save() {
-    packCheckBoxes.forEach(function (checkBox) {
-        localStorage.setItem(checkBox.id, checkBox.checked);
-    });
-}
+function save(array) {
+    array.forEach(function (checkBox) {
+                localStorage.setItem(checkBox.id, checkBox.checked);
 
-function load() {
-    packCheckBoxes.forEach(function (checkBox) {
-        let checked = JSON.parse(localStorage.getItem(checkBox.id));
-        document.getElementById(checkBox.id).checked = checked;
     })
 
+
+
+    //  packCheckBoxes.forEach(function (checkBox) {
+    //      localStorage.setItem(checkBox.id, checkBox.checked);
+    //  });
+}
+
+//make sure at least one category is checked
+function checkCategories() {
+    var pack = isChecked(packCheckBoxes);
+    if (pack) {
+        save(packCheckBoxes)
+        var category = isChecked(categoryCheckBoxes);
+        if (category) {
+            save(categoryCheckBoxes);
+        } else {
+            alert("please select a category")
+        }
+    } else {
+        alert("please select a pack")
+    }
+
+}
+
+function isChecked(array) {
+    let notChecked = 0
+    array.forEach(function (checkBox) {
+        if (checkBox.checked == false) {
+            notChecked += 1;
+        }
+    })
+
+    //all unchecked
+    if (notChecked == categoryCheckBoxes.length) {
+        //alert("you must select a category");
+        return false;
+    } else {
+        //save()
+        return true
+    }
+}
+
+//load whether checkboxes where checked or unchecked
+function load() {
+
+    [].forEach.call(checkBoxes, function (checkBox) {
+        let checked = JSON.parse(localStorage.getItem(checkBox.id));
+        document.getElementById(checkBox.id).checked = checked;
+    });
 }
 
 //if basegame only checked uncheck other packs and vice-versa
 function setBasegameOnlyState(element) {
     if ((element.id != "basegame") && (element.checked)) {
         document.getElementById('basegame').checked = false;
-    } else if (element.checked) {
+    } else if (element.id == "basegame" && element.checked) {
         packCheckBoxes.forEach(function (checkBox) {
-            document.getElementById(checkBox.id).checked = false;
+            //don't uncheck basegame
+            if (checkBox.id != "basegame") {
+                document.getElementById(checkBox.id).checked = false;
+            }
         })
     }
 }
 
 
-//create dictonary of cehckbox and their state
+//create dictonary of each ch3ckbox and their state
 function createDictonary() {
     let dict = {};
-    packCheckBoxes.forEach(function (checkBox) {
+    [].forEach.call(checkBoxes, function (checkBox) {
         dict[checkBox.id] = checkBox.checked;
     })
-
-
+    return (dict);
 }
 
+function sendChoices() {
+    let dict = createDictonary();
+    json_dict = JSON.stringify(dict);
+    //might need to add more in body + error handling
+    $.post("/selection", {
+        javascript_data: json_dict
+    });
+
+    $.get("/generate");
+    // $.ajax({
+    //         type: 'POST',
+    //         url: "img/test.py",
+    //         success: function () {
+    //             alert("working")
+    //         },
+    //         error: function () {
+    //             alert("Not Working")
+    //         }
+    //     });
+}
 
